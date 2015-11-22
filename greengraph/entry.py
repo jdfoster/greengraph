0a2cda891cf66c767a6b4e3710e7d9f182d7e99b
Joshua Foster <joshua.david.foster@gmail.com>
2015-11-22 16:36:51 +0000

    Added units test for the command line parser
    Started to add some unit tests and fixtures for the command line
    parser. At present only fails have been added, need to implement passes
    for the command line parser.

--- greengraph/entry.py
+++ greengraph/entry.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python2
-from argparse import ArgumentParser, FileType
-import greengraph
+from argparse import ArgumentParser, Action
+#import greengraph.Greengraph
 
 def entry_point():
     parser = ArgumentParser()
     parser.add_argument('from_arg', metavar='FROM', type=str)
     parser.add_argument('to_arg', metavar='TO', type=str)
-    parser.add_argument('dist_file', metavar='OUT', type=FileType('w'))
+    parser.add_argument('dist_file', metavar='OUT', type=str)
     parser.add_argument('--steps', '-s', type=int, default=20)
     arguments = parser.parse_args()
+
+    for idx, arg in enumerate([arguments.from_arg, arguments.to_arg, arguments.dist_file]):
+        arg_name = ['FROM', 'TO', 'OUT'][idx]
+        arg_len = [2, 2, 5][idx]
+        if len(arg) < arg_len:
+            parser.error(arg_name + ' argument: ' + arg + ', is too short.')
+
+    if arguments.steps < 3:
+        parser.error('STEP argument is below 3.')
+
+    if not arguments.dist_file.lower().endswith('.png'):
+        parser.error('OUT argument: ' +  arguments.dist_file + ', lacks the .png extension.')
+
     print arguments.from_arg, arguments.to_arg, arguments.dist_file, arguments.steps
 
 if __name__ == "__main__":
---
+++ greengraph/tests/test_entry.py
@@ -0,0 +1,48 @@
+import yaml
+import os
+from mock import patch
+import sys
+from nose.tools import assert_raises
+from ..entry import entry_point
+from numpy.random import random, randint, choice
+
+def test_parse_args_fail():
+    # Testing entry_point function using default step value
+    with open(os.path.join(os.path.dirname(__file__), 'fixtures', 'entry_point_fails.yaml')) as fixures_file:
+        fixtures = yaml.load(fixures_file)
+        for fixture in fixtures:
+            test_args = [fixture['program'], fixture['from'], fixture['to'], fixture['out']]
+            test_args = [item if item else '' for item in test_args]
+            with patch.object(sys, 'argv', test_args):
+                with assert_raises(SystemExit) as exception:
+                    entry_point()
+
+def test_parse_args_negative_steps_fail():
+    # Testing entry_point function using random interger below 3 as step value
+    with open(os.path.join(os.path.dirname(__file__), 'fixtures', 'entry_point_passes.yaml')) as fixures_file:
+        fixtures = yaml.load(fixures_file)
+        fixture = fixtures[0]
+        step_values = randint(-20, 3, 10)
+        for step_value in step_values:
+            test_args = [fixture['program'], fixture['from'], fixture['to'], fixture['out']]
+            #switch = choice([' --steps ', '-s ']) # Not working with '--steps'?
+            switch = '-s '
+            test_args = test_args + [switch + str(step_value)]
+            with patch.object(sys, 'argv', test_args):
+                with assert_raises(SystemExit) as exception:
+                    entry_point()
+
+def test_parse_args_float_steps_fail():
+    # Testing entry_point function using random float as step value
+    with open(os.path.join(os.path.dirname(__file__), 'fixtures', 'entry_point_passes.yaml')) as fixures_file:
+        fixtures = yaml.load(fixures_file)
+        fixture = fixtures[0]
+        step_values = random(10)*100
+        #switch = choice([' --steps ', '-s ']) # Not working with '--steps'?
+        switch = '-s '
+        for step_value in step_values:
+            test_args = [fixture['program'], fixture['from'], fixture['to'], fixture['out']]
+            test_args = test_args + [switch + str(step_value)]
+            with patch.object(sys, 'argv', test_args):
+                with assert_raises(SystemExit) as exception:
+                    entry_point()
