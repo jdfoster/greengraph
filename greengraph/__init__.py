27a1805b6a9d6bec01d8115bcb6ca78c21221bfc
Joshua Foster <joshua.david.foster@gmail.com>
2015-11-25 11:20:09 +0000

    Modified __init__.py to import greengraph modules
    The initial script (__init__.py) was modified to allow the import
    relevant modules when using import all ("import *") within the python
    shell. The version number is also included within __init__.py so that it
    is available within the shell environment. Setup.py was modified to
    import from __init__.py.

---
+++ greengraph/__init__.py
@@ -0,0 +1,9 @@
+from greengraph import Greengraph
+from map import Map
+
+__all__ = (
+    "Greengraph",
+    "Map"
+)
+
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
+from greengraph import __version__ as version
+
+DESCRIPTION = """ """ # Place holder for long description
 
 setup(
     name = "greengraph",
-    version = 0.1,
+    version = version,
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/greengraph'],
     install_requires = ['argparse', 'geopy', 'numpy', 'requests', 'matplotlib'],
     author = "Joshua D. Foster",
     author_email = "joshua.foster@ucl.ac.uk",
     description = "Command line tool to return a graph of green space between two geographical locations.",
+    long_description = DESCRIPTION
     license = "MIT",
     keywords = "greenspace geographical graph",
     url = "https://github.com/jdfoster/greengraph"
